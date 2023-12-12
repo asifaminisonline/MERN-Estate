f44c37affb5800a7e0eef8b5c80dd635c131144a
asifaminisonline <asifaminisonline@gmail.com>
2023-12-12 20:55:36 +0530

    Add Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
 function App() {
-  return <h1 className="text-6xl font-bold">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/profile" element={<Profile />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-up" element={<SignOut />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
